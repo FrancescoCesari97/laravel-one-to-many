2723bd93e676a6818b5a2b2db54650b1d0558c0e
FrancescoCesari97 <freedus97@gmail.com>
2024-04-05 17:42:28 +0200

    fatto il seeder dei type ed recuperato l'id di ogni categoria nel seeder dei progetti

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\models\Project;
+use App\models\Type;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
@@ -17,8 +18,12 @@ class ProjectSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
+        // recuperiamo l'id di ogni categoria e lo assegniamo ad ogni progetto
+        $types_id = Type::all()->pluck('id');
+
         for ($i = 0; $i < 150; $i++) {
             $project = new Project();
+            $project->type_id = $faker->randomElement($types_id);
             $project->title = $faker->catchPhrase();
             $project->content = $faker->paragraphs(2, true);
             $project->slug = Str::slug($project->title);
